865de77ded5dd8f642d806482825c6a099a53441
FunnyMika <62733884+FunnyMika@users.noreply.github.com>
2020-07-22 09:48:33 +0800

    Update readAndCheckCsv.py
    save and read profit, also add draw profit function

--- readAndCheckCsv.py
+++ readAndCheckCsv.py
@@ -4,7 +4,11 @@ import time
 import datetime
 import csv
 import os.path
+#画图显示
 import matplotlib.pyplot as plt
+from pylab import mpl #正常显示画图时出现的中文
+mpl.rcParams['font.sans-serif']=['SimHei'] #这里使用微软雅黑字体
+mpl.rcParams['axes.unicode_minus']=False #画图时显示负号
 
 g_profitFileName = 'C:/python/csv/zhangting/profit.csv'
 g_dirProfit = {}
@@ -19,15 +23,19 @@ def saveParaToCsv(ts_code, date, profit):
 
 def calculateProfit(fileName):
     global g_dirProfit
+    g_dirProfit = {}
     with open(fileName, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            if row['date'] not in g_dirProfit:
-                g_dirProfit[row['date']] = float(row['profit'])
+            date = row['date']
+            value = row['profit']
+            if date not in g_dirProfit:
+                g_dirProfit[date] = float(value)
             else:
-                g_dirProfit[row['date']] = g_dirProfit[row['date']] + float(row['profit'])
-    g_dirProfit = sorted(g_dirProfit.items(), key=lambda d: d[0], reverse=False)
-    print(g_dirProfit)
+                g_dirProfit[date] = g_dirProfit[date] + float(value)
+    #print(g_dirProfit)
+    g_dirProfit = dict(sorted(g_dirProfit.items(), key=lambda d: d[0], reverse=False))
+    #print(g_dirProfit)
 
 def saveFile():
     saveParaToCsv('000020.SZ', '2020-01-01', 3)
@@ -37,23 +45,22 @@ def saveFile():
     saveParaToCsv('000040.SZ', '2020-01-02', 33)
     saveParaToCsv('000020.SZ', '2020-01-01', 20)
 
-calculateProfit(g_profitFileName)
-
-def drawPic():
+def drawProfitPic():
     print(g_dirProfit)
+    totalProfit = 0
+    averageProfit = 0
     x1 = list(g_dirProfit.keys())
     y1 = list(g_dirProfit.values())
     plt.plot(x1, y1, label='Frist line', linewidth=3, color='r', marker='o',
              markerfacecolor='blue', markersize=12)
-    plt.xlabel('Plot Number')
-    plt.ylabel('Important var')
-    plt.title('Interesting Graph\nCheck it out')
+    plt.xlabel('日期')
+    plt.ylabel('收益率')
+    for i in range(len(y1)):
+        totalProfit += y1[i]
+    averageProfit = round(totalProfit / len(y1), 2)
+    plt.title(str(len(y1)) + '天' + '平均收益率: ' + str(averageProfit))
     plt.legend()
     plt.show()
 
-dic = {}
-a = 'lyw'
-dic[a] = 2
-print(dic)
-print(list(dic.keys()))
-#drawPic()
+calculateProfit(g_profitFileName)
+drawProfitPic()
