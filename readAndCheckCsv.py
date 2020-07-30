14b891945cd3a9cb6fcc57762669c0a7d8d40209
FunnyMika <62733884+FunnyMika@users.noreply.github.com>
2020-07-30 21:09:35 +0800

    Update readAndCheckCsv.py

--- readAndCheckCsv.py
+++ readAndCheckCsv.py
@@ -13,18 +13,30 @@ mpl.rcParams['axes.unicode_minus']=False #画图时显示负号
 g_profitFileName = 'C:/python/csv/zhangting/profit.csv'
 g_dirProfit = {}
 #('000020.SZ', '2020-01-01', 9.5)
-def saveParaToCsv(ts_code, date, profit):
+def saveProfitToCsv(ts_code, date, profit):
+    writeMethod = 'w'
     isFileExist = os.path.isfile(g_profitFileName)
-    with open(g_profitFileName, 'a', encoding='utf-8') as f:
+    if False == isFileExist:
+        writeMethod = 'w'
+    else:
+        writeMethod = 'a'
+
+    with open(g_profitFileName, writeMethod, encoding='utf-8') as f:
         writer = csv.writer(f)
         if False == isFileExist:
             writer.writerow(['ts_code', 'date', 'profit'])
         writer.writerow([ts_code, date, profit])
 
+def deleteProfitToCsv(name):
+    if True == os.path.isfile(name):
+        os.remove(name)
+
 def calculateProfit(fileName):
     global g_dirProfit
     g_dirProfit = {}
     dirtCount = {}
+    if False == os.path.isfile(fileName):
+        return;
     with open(fileName, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -39,21 +51,24 @@ def calculateProfit(fileName):
     #print(g_dirProfit)
     g_dirProfit = dict(sorted(g_dirProfit.items(), key=lambda d: d[0], reverse=False))
     for k in g_dirProfit:
-        g_dirProfit[k] = round((g_dirProfit[k] * 100) / dirtCount[k], 4)
+        g_dirProfit[k] = round((g_dirProfit[k]) / dirtCount[k], 4)
     #print(g_dirProfit)
 
-def saveFile():
-    saveParaToCsv('000020.SZ', '2020-01-01', 3)
-    saveParaToCsv('000030.SZ', '2020-01-01', 5)
-    saveParaToCsv('000020.SZ', '2020-01-03', 8)
-    saveParaToCsv('000030.SZ', '2020-01-03', 10)
-    saveParaToCsv('000040.SZ', '2020-01-02', 33)
-    saveParaToCsv('000020.SZ', '2020-01-01', 20)
+def saveFileTest():
+    saveProfitToCsv('000020.SZ', '2020-01-01', 3)
+    saveProfitToCsv('000030.SZ', '2020-01-01', 5)
+    saveProfitToCsv('000020.SZ', '2020-01-03', 8)
+    saveProfitToCsv('000030.SZ', '2020-01-03', 10)
+    saveProfitToCsv('000040.SZ', '2020-01-02', 33)
+    saveProfitToCsv('000020.SZ', '2020-01-01', 20)
 
 def drawProfitPic():
-    print(g_dirProfit)
+    print(f'drawProfitPic,  g_dirProfit = {g_dirProfit}')
     totalProfit = 0
     averageProfit = 0
+    if 0 == len(g_dirProfit):
+        return;
+
     x1 = list(g_dirProfit.keys())
     y1 = list(g_dirProfit.values())
     plt.plot(x1, y1, label='Frist line', linewidth=3, color='r', marker='o',
@@ -67,5 +82,6 @@ def drawProfitPic():
     plt.legend()
     plt.show()
 
-calculateProfit(g_profitFileName)
-drawProfitPic()
+#calculateProfit(g_profitFileName)
+#drawProfitPic()
+#deleteProfitToCsv(g_profitFileName)
