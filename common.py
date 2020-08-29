6c7c27da5f0a6aa29cbcea593dd76a9adbbf074d
FunnyMika <62733884+FunnyMika@users.noreply.github.com>
2020-08-29 10:50:24 +0800

    add download function
    add function that can download surged stock and ma10 black stocks to json file.

--- common.py
+++ common.py
@@ -9,6 +9,8 @@ import time
 import datetime
 import os.path
 import os
+import json
+
 
 ts.set_token('85a6e863fa91060204e5339228932e52c4f90863d773778f3040f14a')
 
@@ -23,10 +25,10 @@ def getLastTradeDate():
     return df.iloc[0,1]
 
 #获取所有股票,1)非深圳，上海股票剔除 2)ST股票剔除
-def getAllStocks():
+def getAllStocks(date):
     listAllStocks = []
     stList = []
-    date = str(time.strftime("%Y%m%d", time.localtime()))
+    #date = str(time.strftime("%Y%m%d", time.localtime()))
 
     pro = ts.pro_api()
     try:
@@ -50,13 +52,19 @@ def getAllStocks():
             stList.append(row['ts_code'])
     #把ST股票从保存的股票里面剔除
     listAllStocks = list(set(listAllStocks).difference(stList))
+    
+    print(f'AllStocks num = {len(listAllStocks)}, ST: num={len(stList)}')
+    #print(listAllStocks)
 
+    return listAllStocks
+
+#600250.SH  --> sh600250
+def converStockList(listAllStocks):
     tempList = []
     for key in listAllStocks:
         convertStock = convertStockId(key)
         tempList.append(convertStock)
     listAllStocks = tempList.copy()
-    print(f'AllStocks num = {len(listAllStocks)}, ST: num={len(stList)}')
 
     return listAllStocks
 
@@ -71,9 +79,9 @@ def convertStockId(s1):
     return s1
 
 #获取昨天涨停的股票
-def getSurgedStocks():
+def getSurgedStocks(date):
     listSurged = []
-    date = getLastTradeDate()
+    #date = getLastTradeDate()
     pro = ts.pro_api()
     df = pro.limit_list(trade_date=date, limit_type='U', fields = 'ts_code, name')
     for index, row in df.iterrows():
@@ -82,27 +90,62 @@ def getSurgedStocks():
     print(f'Yesterday surged stock number = {len(listSurged)}')
     return listSurged
 
+def downloadSurgedStockListByTushare(date):
+    todaySurgedDict = {}
+    fileName = os.getcwd() + '\TodaySurgedStockList.json'
+    fileName = fileName.replace('\\', '/')
+    stockList = getSurgedStocks(date)
+    if len(stockList) > 0:
+        for stock in stockList:
+            todaySurgedDict.update({stock: 0})
+            
+        with open(fileName, 'w', encoding='utf8') as fp:
+            json.dump(todaySurgedDict, fp, ensure_ascii=False)
+            print(f'Saved {fileName} successfully!')
+            
 #获取收盘价 > 10日均线 1.05倍的黑名单
-def getMa10BlackList(list1):
+def getMa10BlackList(date, list1):
     ma10BlackList = []
-    lastDate = getLastTradeDate()
+    #lastDate = getLastTradeDate()
+    print(f'getMa10BlackList')
+
     for key in list1:
-        df = ts.pro_bar(ts_code=key, start_date='20200811', end_date=lastDate,ma=[10])
+        df = ts.pro_bar(ts_code=key, start_date='20200814', end_date=date,ma=[10])
         for index, row in df.iterrows():
             close = row['close']
             ma10 = row['ma10']
-            print(row['ma10'])
+            #print(row['ma10'])
             #收盘价 > 10日均线 1.05倍
             if float(close) > (float(ma10) * 1.05):
                 ma10BlackList.append(key)
+                print(f'black stock = {key}')
             break
     return ma10BlackList
 
-#getSurgedStocks()
-startDate = '20200827'
-endDate = '20190110'
-g_dailyCsv = 'C:/python/csv/temp/mini/'
-list1 = getAllStocks()
+def downloadMa10BlackListByTushare(date):
+    allStockList = []
+    todayAbove10DayAverageDict = {}
+    fileName = os.getcwd() + '\Above10DayAverageList.json'
+    fileName = fileName.replace('\\', '/')
+
+    #allStockList = getAllStocks(date)
+    #allStockList = getMa10BlackList(date, allStockList)
+    allStockList = ['002979.SZ', '600610.SH']
+    allStockList = converStockList(allStockList)
+    if len(allStockList) > 0:
+        for stock in allStockList:
+            todayAbove10DayAverageDict.update({stock: 0})
+            
+        with open(fileName, 'w', encoding='utf8') as fp:
+            json.dump(todayAbove10DayAverageDict, fp, ensure_ascii=False)
+            print(f'Saved {fileName} successfully!')
+
+date = '20200828'
+#downloadSurgedStockListByTushare(date)
+downloadMa10BlackListByTushare(date)
+
+#getSurgedStocks(date)
+#list1 = getAllStocks(date)
 #print(list1)
 
 
